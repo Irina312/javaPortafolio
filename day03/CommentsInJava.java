24cdc835e7977585acc2b6abf2d1c2b74e342e71
ISE312 <irina.evans@yahoo.com>
2022-04-10 17:38:52 -0400

    day 03

---
+++ day03/CommentsInJava.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class CommentsInJava {
+	public static void main(String[] args) {
+		// this is my comment - compiler will ignore the comments
+		
+		/*
+		 * My bigger comments are here
+		 */
+		
+		/**
+		 * Most of the time this type of comments are used to document the code
+		 */
+	}
+
+}
---
+++ day03/EscapedChars.java
@@ -0,0 +1,12 @@
+package day03;
+
+public class EscapedChars {
+	public static void main(String[] args) {
+		System.out.println("I said \"Hello\"");
+		System.out.println("John\tDoe");
+		System.out.println("John\nDoe");
+		System.out.println("A\nB\nC");
+		System.out.println("A\tB\tC");
+	}
+
+}
